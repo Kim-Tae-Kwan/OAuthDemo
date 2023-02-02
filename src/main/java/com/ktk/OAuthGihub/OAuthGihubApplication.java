5928ea9bf918c6cd7c4acababe292a10747db3aa
Kim-Tae-Kwan <vodjaos@gmail.com>
2023-02-02 18:02:09 +0900

    github OAuth yml 설정 까지

---
+++ src/main/java/com/ktk/OAuthGihub/OAuthGihubApplication.java
@@ -0,0 +1,13 @@
+package com.ktk.OAuthGihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthGihubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthGihubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktk/OAuthGihub/OAuthGihubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktk.OAuthGihub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthGihubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
